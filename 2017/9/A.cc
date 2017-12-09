d00960e6ead546639dbb671880326ed6f649c0ad
Xin Wei Chow <chowxinwei@gmail.com>
2017-12-09 16:09:09 +1100

    Add solutions for Day 9

---
+++ 2017/9/A.cc
@@ -0,0 +1,33 @@
+#include "../../util.hpp"
+
+int main() {
+    std::ios_base::sync_with_stdio(false); cin.tie(0);
+    string line;
+    cin >> line;
+    int open = 0;
+    int n = line.size();
+
+    bool garbage = false;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        char c = line[i];
+        if (!garbage) {
+            if (c == '{') {
+                open++;
+                sum += open;
+            } else if (c == '}') {
+                open--;
+            } else if (c == '<') {
+                garbage = true;
+            }
+        } else {
+            if (c == '!') {
+                i++;
+            } else if (c == '>') {
+                garbage = false;
+            } 
+        }
+    }
+    cout << sum << endl;
+}
+
