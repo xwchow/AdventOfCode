e5b63c99525b579af446154f1e3bdceaf5d2c825
Xin Wei Chow <chowxinwei@gmail.com>
2017-12-15 16:06:02 +1100

    Add solutions for Day 15

---
+++ 2017/15/A.cc
@@ -0,0 +1,20 @@
+#include "../../util.hpp"
+
+bool match(int A, int B) {
+    int low = (1 << 16) - 1;
+    return (A & low) == (B & low);
+}
+int main() {
+    std::ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int A = 618, B = 814;
+    int count = 0;
+    for (int i = 0; i < 40000000; i++) {
+        A = (ll) A * 16807 % 2147483647;
+        B = (ll) B * 48271 % 2147483647;   
+
+        count += match(A, B);
+    }
+    cout << count << endl;
+}
+
---
+++ 2017/15/B.cc
@@ -0,0 +1,24 @@
+#include "../../util.hpp"
+
+bool match(int A, int B) {
+    int low = (1 << 16) - 1;
+    return (A & low) == (B & low);
+}
+int main() {
+    std::ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int A = 618, B = 814;
+    int count = 0;
+    for (int i = 0; i < 5000000; i++) {
+        do {
+            A = (ll) A * 16807 % 2147483647;
+        } while (A % 4);
+        do {
+            B = (ll) B * 48271 % 2147483647;   
+        } while (B % 8);
+
+        count += match(A, B);
+    }
+    cout << count << endl;
+}
+
