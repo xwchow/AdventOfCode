9339fb1138f8a119d7ea6ea229511f189a7a7b5f
Xin Wei Chow <chowxinwei@gmail.com>
2017-12-15 19:23:05 +1100

    Reimplement Day 11 using hexagon cube coords

--- 2017/11/B.cc
+++ 2017/11/B.cc
@@ -1,33 +1,43 @@
 #include "../../util.hpp"
 
-const string DIR[] = {"n", "ne", "se", "s", "sw", "nw"};
-int dx[] = {-2, -1, 1, 2, 1, -1};
-int dy[] = {0, 1, 1, 0, -1, -1};
 
-int steps(int x, int y) {
-    x = abs(x);
-    y = abs(y);
-    int m = min(x, y);
-    x -= m;
-    y -= m;
-    int sum = m + x / 2 + y;
-    return sum;
-}
+struct Hex {
+    int x, y, z;
+    
+    Hex() {}
+    Hex(int x, int y, int z) : x(x), y(y), z(z) {}
+    
+    bool operator<(const Hex& o) const {
+        return x < o.x || (x == o.x && y < o.y) || (x == o.x && y == o.y && z < o.z);
+    }
+    
+    int dist() { return (abs(x) + abs(y) + abs(z)) / 2; }
+    Hex operator+(const Hex& o) const { return Hex(x + o.x, y + o.y, z + o.z); }
+    Hex& operator+=(const Hex& o) { x += o.x; y += o.y; z += o.z; return *this; }
+    Hex operator-(const Hex& o) const { return Hex(x - o.x, y - o.y, z - o.z); }
+    Hex& operator-=(const Hex& o) { x -= o.x; y -= o.y; z -= o.z; return *this; }
+};
+
+const string DIR[] = {"n", "ne", "se", "s", "sw", "nw"};
+Hex nbr[6] = { Hex(-1, 1, 0),
+               Hex(0, 1, -1),
+               Hex(1, 0, -1),
+               Hex(1, -1, 0),
+               Hex(0, -1, 1),
+               Hex(-1, 0, 1) };
 
 int main() {
     std::ios_base::sync_with_stdio(false); cin.tie(0);
 
-    string line;
-    cin >> line;
+    string line;     cin >> line;
     vs words = split(line, ',');
+    Hex coord(0, 0, 0);
 
-    int x = 0, y = 0;
     int Ma = 0;
     for (string w : words) {
-        int idx = find(DIR, DIR + 6, w) - DIR;
-        x += dx[idx];
-        y += dy[idx];
-        Ma = max(Ma, steps(x, y));
+        int i = find(DIR, DIR + 6, w) - DIR;
+        coord += nbr[i];
+        Ma = max(Ma, coord.dist());
     }
     
     cout << Ma << endl;
