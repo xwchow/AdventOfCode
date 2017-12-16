060bfbf735d85fbfb6eda6d78eac61577d8d352d
Xin Wei Chow <chowxinwei@gmail.com>
2017-12-16 16:26:58 +1100

    Add solutions for Day 16

---
+++ 2017/16/B.cc
@@ -0,0 +1,31 @@
+#include "../../util.hpp"
+
+int main() {
+    std::ios_base::sync_with_stdio(false); cin.tie(0);
+
+    string line; cin >> line;
+    vs words = split(line, ',');
+    string ori = "abcdefghijklmnop";
+    string progs = "abcdefghijklmnop";
+    for (int i = 0; i < 1000000000 % 60; i++) {
+        for (string w : words) {
+            if (w[0] == 's') {
+                int rot = stoi(w.substr(1)) % 16;
+                rotate(progs.begin(), progs.begin() + 16 - rot, progs.end());
+            } else if (w[0] == 'x') {
+                vi v = parseInt(split(w.substr(1), '/'));
+                int A = v[0], B = v[1];
+                swap(progs[A], progs[B]);
+            } else if (w[0] == 'p') {
+                char a = w[1], b = w[3];
+                int A = progs.find(a), B = progs.find(b);
+                swap(progs[A], progs[B]);
+            }
+        }
+        if (progs == ori) {
+            cerr << i + 1 << endl; // cycle length 
+        }
+    }
+    cout << progs << endl;
+}
+
