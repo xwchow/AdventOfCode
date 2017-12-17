0ae00c864c8f7a3921900c57426d8278c8082166
Xin Wei Chow <chowxinwei@gmail.com>
2017-12-17 16:54:09 +1100

    Remove debug line

--- 2017/17/A.cc
+++ 2017/17/A.cc
@@ -4,6 +4,8 @@ int main() {
     std::ios_base::sync_with_stdio(false); cin.tie(0);
 
     int n; cin >> n;
+    int k = 2017;
+    
     int pos = 0;
     vi v = {0};
     int last = 0;
@@ -12,7 +14,6 @@ int main() {
         v.insert(v.begin() + pos, i);
         last = v[(pos + 1) % v.size()];
     }
-    debug(v);
     cout << last << endl;
 }
 
--- 2017/17/B.cc
+++ 2017/17/B.cc
@@ -4,10 +4,10 @@ int main() {
     std::ios_base::sync_with_stdio(false); cin.tie(0);
 
     int n; cin >> n;
-    int pos = 0;
-
-    int last = 0;
     int k = 50000000;
+    
+    int pos = 0;
+    int last = 0;
 
     for (int i = 1; i <= k; i++) {
         pos = (pos + n) % i;
