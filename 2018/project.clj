bc6e51f7c2bec801878e8b5ac6d4d3717079802a
Xin Wei Chow <xinweic@xinweic-macbookpro.roam.corp.google.com>
2018-12-01 17:08:11 +1100

    Add project.clj file.

---
+++ 2018/project.clj
@@ -0,0 +1,7 @@
+(defproject aoc-2018 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-exec "0.3.7"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]])
